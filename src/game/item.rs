224191b4b0cb952b80086341edcd359855a0d001
Christopher Boyle <chrismjboyle@gmail.com>
2022-02-07 10:06:30 +0000

    Move item description to item.rs

--- src/game/item.rs
+++ src/game/item.rs
@@ -6,3 +6,54 @@ pub struct Item {
     pub description: String,
     pub hidden: Option<bool>
 }
+
+fn get_article(word: &String) -> String {
+    let a = "a".to_string();
+    let an = "an".to_string();
+    match &word[0..1] {
+        "a" => an,
+        "e" => an,
+        "i" => an,
+        "o" => an,
+        "u" => an,
+        _ => a
+    }
+
+}
+
+impl Item {
+
+    pub fn items_to_text(items: &Vec<String>) -> Option<String> {
+        if items.len() == 0 {
+            None
+        }
+        else if items.len() == 1 {
+            Some(get_article(&items[0]) + " " + &items[0].clone())
+        }
+        else {
+            let mut s = String::new();
+            for i in 0..items.len() - 1 {
+                s = s + &get_article(&items[i]) + " " + &items[i] + ", ";
+            }
+            let last = &items.last().unwrap();
+            s = s + "and " + &get_article(last) + " " + last;
+            Some(s)
+        }
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+
+    use crate::game::item::*;
+
+    #[test]
+    fn test_get_article() {
+        let a = "a".to_string();
+        let an = "an".to_string();
+        assert_eq!(get_article(&"foo".to_string()), a);
+        assert_eq!(get_article(&"abacus".to_string()), an);
+        assert_eq!(get_article(&"yellow".to_string()), a);
+    }
+}
--- src/game/stage.rs
+++ src/game/stage.rs
@@ -3,26 +3,13 @@ use std::collections::HashMap;
 use serde::Deserialize;
 
 use super::path::Path;
+use super::item::Item;
 
 #[derive(Deserialize)]
 pub struct Stage {
     description: String,
     paths: HashMap<String, Path>,
     items: Option<Vec<String>>
-}
-
-fn get_article(word: &String) -> String {
-    let a = "a".to_string();
-    let an = "an".to_string();
-    match &word[0..1] {
-        "a" => an,
-        "e" => an,
-        "i" => an,
-        "o" => an,
-        "u" => an,
-        _ => a
-    }
-
 }
 
 impl Stage {
@@ -33,24 +20,9 @@ impl Stage {
 
     pub fn search(&self) {
         if let Some(ref items) = self.items {
-            let n = items.len();
-            if n == 1 {
-                let ref item = items[0];
-                println!("\nYou found {} {}.", get_article(&item), item);
-            }
-            else if n == 2 {
-                let ref item1 = items[0];
-                let ref item2 = items[1];
-                println!("\nYou found {} {} and {} {}.", get_article(&item1), item1, get_article(&item2), item2);
-            }
-            else if n > 2 {
-                println!("\nYou found:");
-                for item in items {
-                    println!(" - {} {}", get_article(&item), item);
-                }
-            }
-            else {
-                println!("\nYou search the area, but find nothing.");
+            match Item::items_to_text(items) {
+                Some(txt) => println!("\nYou found {}", txt),
+                None => println!("\nYou search the area, but find nothing.")
             }
         }
         else {
@@ -58,25 +30,33 @@ impl Stage {
         }
     }
 
-    pub fn take(&self, item_name: String) {
+    pub fn take_all(&mut self) -> Option<Vec<String>> {
+        if let Some(items) = &mut self.items {
+            let items_cpy = items.clone();
+            items.clear();
+            Some(items_cpy)
+        }
+        else {
+            None
+        }
+    }
 
+    pub fn take(&mut self, item_name: String) -> Option<String> {
+        if let Some(items) = &mut self.items {
+            if items.contains(&item_name) {
+                items.retain(|x| x != &item_name);
+                Some(item_name)
+            }
+            else {
+                None
+            }
+        }
+        else {
+            None
+        }
     }
 
     pub fn get_path(&self, path_ident: &String) -> Option<&Path> {
         self.paths.get(path_ident)
     } 
 } // impl stage
-
-mod tests {
-
-    use crate::game::stage::*;
-
-    #[test]
-    fn test_get_article() {
-        let a = "a".to_string();
-        let an = "an".to_string();
-        assert_eq!(get_article(&"foo".to_string()), a);
-        assert_eq!(get_article(&"abacus".to_string()), an);
-        assert_eq!(get_article(&"yellow".to_string()), a);
-    }
-}
